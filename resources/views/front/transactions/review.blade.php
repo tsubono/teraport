4618175d5f4e03c956928268350cfc12b13708da
skfm <skkasans@gmaiul.com>
2020-10-31 15:52:15 +0000

    評価機能を実装

--- resources/views/front/transactions/review.blade.php
+++ resources/views/front/transactions/review.blade.php
@@ -16,7 +16,41 @@
                 <form action="{{ route('front.transactions.review.store', ['transaction'=> $transaction]) }}" method="post">
                     @csrf
 
-                    <button type="submit" class="submit-btn">登録する</button>
+                    <div class="form-group">
+                        <label for="content">内容</label>
+                        <textarea name="content" rows="10"></textarea>
+
+                        @error('content')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
+
+                    <div class="form-group">
+                        <label for="rate">評価</label>
+                        <div class="rate-form">
+                            <input id="star5" type="radio" name="rate" value="5">
+                            <label for="star5">★</label>
+                            <input id="star4" type="radio" name="rate" value="4">
+                            <label for="star4">★</label>
+                            <input id="star3" type="radio" name="rate" value="3">
+                            <label for="star3">★</label>
+                            <input id="star2" type="radio" name="rate" value="2">
+                            <label for="star2">★</label>
+                            <input id="star1" type="radio" name="rate" value="1">
+                            <label for="star1">★</label>
+                        </div>
+                        @error('rate')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+                    </div>
+
+                    <div class="form-group">
+                        <button type="submit" class="submit-btn register-btn">登録する</button>
+                    </div>
                 </form>
             </div>
         </section>
