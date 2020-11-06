2536add48b74ddaa51fe437b3a94299704fa3b15
skfm <skkasans@gmaiul.com>
2020-11-06 11:30:04 +0000

    売上申請一覧ページの作成

--- resources/views/admin/sale-requests/index.blade.php
+++ resources/views/admin/sale-requests/index.blade.php
@@ -11,7 +11,54 @@
         <section class="admin-sale-request-section">
             <div class="container">
                 <h2>売上申請一覧</h2>
+
+                <div class="sale-request-wrap">
+                    <ul class="sale-request-list">
+                        @forelse($saleRequests as $saleRequest)
+                        <li class="sale-request-item">
+                            <div class="left-item">
+                                <div class="left-img">
+                                    <img src="{{ $saleRequest->user->display_icon_image_path }}" alt="アイコン" class="user-image">
+                                </div>
+                                <div class="fullname">
+                                    <p>{{ $saleRequest->user->name }}</p>
+                                </div>
+                                <div class="transfer-wrap">
+                                    <div class="transfer-information">
+                                        <p>
+                                            振り込み金額:{{ number_format($saleRequest->price) }}円
+                                        </p>
+                                        <p>
+                                            振り込み期限:{{ date('Y年m月d日',  strtotime($saleRequest->transfer_limit_date)) }}
+                                        </p>
+                                    </div>
+                                    <p class="account">
+                                        振り込み口座:{{ ($saleRequest->bank_name) }} {{ ($saleRequest->branch_name) }} {{ ($saleRequest->bank_number) }} {{ ($saleRequest->account_holder) }}
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="right-item">
+                                @if ($saleRequest->status === 1)
+                                <p class="transfer-done">
+                                    振り込み済み
+                                </p>
+                                @else
+                                <a href="{{ route('admin.sale-requests.update', ['saleRequest' => $saleRequest]) }}" class="transfer-yet">
+                                    振り込み済みにする
+                                </a>
+                                @endif
+                            </div>
+                        </li>
+                        @empty
+                        <li>
+                            <div class="margin-auto">見つかりませんでした</div>
+                        </li>
+                        @endforelse
+                    </ul>
+                    {{ $saleRequests->appends(['c' => $params['c'] ?? '', 'keyword' => $params['keyword'] ?? ''])->links('pagination::default') }}
+                </div>
             </div>
+
         </section>
     </div>
 @endsection
