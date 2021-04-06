233ac5753cf16fd3668526423a91ddca1c8685c4
tsubono <tsubono@wss.systems>
2021-04-06 21:10:59 +0900

    画像圧縮

--- app/Http/Controllers/Api/FileController.php
+++ app/Http/Controllers/Api/FileController.php
@@ -15,10 +15,19 @@ class FileController extends Controller
      * @param Request $request
      * @return \Illuminate\Http\JsonResponse
      */
-    public function uploadImage(Request $request) {
+    public function uploadImage(Request $request)
+    {
         $dir = $request->get('dir');
-        $filename = Carbon::now()->format('YmdHis').rand(1, 9).".".$request->file('img')->extension();
-        $path = $request->file('img')->storeAs($dir, $filename, 'public');
-        return response()->json(['status' => 'ok', 'path' => Storage::url($path)]);
+        $img = $request->file('img');
+        $filename = Carbon::now()->format('YmdHis') . rand(1, 9) . "." . $img->extension();
+        $image = \Image::make($img)->setFileInfoFromPath($img);
+        $image->orientate()->resize(1200, null,
+            function ($constraint) {
+                $constraint->aspectRatio();
+                $constraint->upsize();
+            }
+        );
+        $image->save(storage_path() . "/app/public/{$dir}/{$filename}");
+        return response()->json(['status' => 'ok', 'path' => Storage::url("{$dir}/{$filename}")]);
     }
 }
