bb1b6099a5aba21aded7139c041b619d8061a51d
tsubono <tsubono@wss.systems>
2021-04-06 21:19:53 +0900

    mix変更

--- resources/views/front/components/js.blade.php
+++ resources/views/front/components/js.blade.php
@@ -1,6 +1,6 @@
 <script src="{{ secure_asset('asset/js/jquery-3.5.1.min.js') }}"></script>
 @if (!request()->is('services/*'))
-    <script src="{{ secure_asset('js/app.js') }}"></script>
+    <script src="{{ mix('js/app.js') }}"></script>
 @endif
 <script>
     $(function() {
